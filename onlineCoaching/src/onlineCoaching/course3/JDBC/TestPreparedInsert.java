9466e69ae0b04c9a49e59e3f647c88e845b16007
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2019-12-20 00:33:01 +0530

    problem

---
+++ onlineCoaching/src/onlineCoaching/course3/JDBC/TestPreparedInsert.java
@@ -0,0 +1,54 @@
+package onlineCoaching.course3.JDBC;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class TestPreparedInsert {
+	public static void main(String[] args) throws SQLException {
+		Connection conn =  DBUtil.getConnection(DBType.ORADB);
+		
+		int employee_id;
+		String employee_name, email;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter Employee ID : ");
+		employee_id = Integer.parseInt(scanner.nextLine());
+		
+		System.out.println("Enter Employee Name : ");
+		employee_name = scanner.nextLine();
+		
+		System.out.println("Enter Email : ");
+		email = scanner.nextLine();
+		
+		String sql = "insert into newemployee values(?,?,?)";
+		
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		
+		pstmt.setInt(1, employee_id);
+		pstmt.setString(2, employee_name);
+		pstmt.setString(3, email);
+		
+		int result = pstmt.executeUpdate();
+		
+		if(result == 1) {
+			System.out.println("Record Inserted Successfully");
+		}else {
+			System.out.println("Error");
+		}
+		
+		scanner.close();
+		pstmt.close();
+		conn.close();
+	}
+}
+
+
+
+
+
+
+
---
+++ onlineCoaching/src/onlineCoaching/course3/JDBC/UpdatableResultSetDemo.java
@@ -0,0 +1,41 @@
+package onlineCoaching.course3.JDBC;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class UpdatableResultSetDemo {
+	public static void main(String[] args) throws SQLException {
+		try (
+				Connection conn = DBUtil.getConnection(DBType.ORADB);
+				Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
+				ResultSet rs = stmt.executeQuery("select Department_Id,Department_Name,Manager_Id,Location_Id from Departments");
+				//we should remember that a query cannot use a select *, and also it is mandatory to select all the non-null columns
+				//and a query must select single table set only, no joins
+				
+				
+				){
+			rs.absolute(2);
+			rs.updateString("Department_Name", "Information Technology");
+			rs.updateRow();
+			
+			System.out.println("Record Updated Successfully");
+			
+			rs.moveToInsertRow();
+			rs.updateInt("Department_Id", 999);
+			rs.updateString("Department_Name", "Training");
+			rs.updateInt("Manager_Id", 200);
+			rs.updateInt("Location_Id", 1800);
+			rs.insertRow();
+			
+			System.out.println("Record Inserted Successfully");
+			
+		}catch(SQLException e) {
+			DBUtil.showErrorMessage(e);
+		}
+	}
+}
+
+
+
